8b86db1c4b7e20fda186a2194d90c82ba69fbe56
yuvrajsinghnegi012 <144053547+yuvrajsinghnegi012@users.noreply.github.com>
2024-08-12 15:02:59 +0530

    initiated project added shadcn

---
+++ app/(dashboard)/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-sky-600">
+      <div>Hello, Next.js</div>
+      <Button variant="destructive">Button</Button>
+    </div>
+  );
+}
